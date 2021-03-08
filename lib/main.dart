77c4055f1abd088db029ba6aba73cf3f2746d44d
jwdevelopper <58191292+jwdevelopper@users.noreply.github.com>
2021-03-08 12:31:03 -0300

    Problema corrigido de conexão com firebase e teste de operações

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,32 @@ void main() async{
   //Firestore.instance.collection("col").document("doc").setData({"texto":"daniel"});
   await Firebase.initializeApp();
   CollectionReference teste = FirebaseFirestore.instance.collection('col');
-  DocumentReference doc = FirebaseFirestore.instance.doc('doc');
-  doc.set({'doc': 'doc'}).then((valor) => print("criou o documento"))
+  DocumentReference doc = FirebaseFirestore.instance.collection('col').doc('msg2');
+  //DocumentReference doc = FirebaseFirestore.instance.collection('col').doc('msg2').collection('arquivos').doc();
+  /*doc.set({'doc': 'doc'}).then((valor) => print("criou o documento"))
   .catchError((error) => print("falha na conexão: $error"));
-  print('Depois da conexao');
-  teste.add({'texto':'Junior'}).then((valor) => print("Conexão estabelecida"))
-  .catchError((error) => print("falha na conexão: $error"));
+  print('Depois da conexao');*/
+  /*doc.set({
+    'texto':'Tudo bem?',
+    'from':'Junior',
+    'read':false
+    }).then((valor) => print("Conexão estabelecida"))
+  .catchError((error) => print("falha na conexão: $error"));*/
+  /*doc.update({
+    'read':true
+    }).then((valor) => print("Conexão estabelecida"))
+  .catchError((error) => print("falha na conexão: $error"));*/
+  /*doc.set({
+    'arqname':'foto.png'
+    }).then((valor) => print("Conexão estabelecida"))
+  .catchError((error) => print("falha na conexão: $error"));*/
+  /*QuerySnapshot snapshot = await teste.get();
+  snapshot.docs.forEach((d) {
+    print('Entrou no foreach');
+    print(d.data());
+  });*/
+  DocumentSnapshot snapshot = await doc.get();
+  print(snapshot.data());
 }
 
 class MyApp extends StatelessWidget {
@@ -30,7 +50,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold()
+      home: Container()
     );
   }
 
